9c06b8a1005aa9613ab2568ef437a72532f56de6
JvTadeo <joao.tadeot2@gmail.com>
2022-12-05 00:51:29 -0300

    Aprendendo o innerHTML e DOM

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,4 +1,40 @@
-class CalController
+class CalcController
 {
+    constructor()
+    {
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize()
+    {
+        let displayCalcEl = document.querySelector("#display");
+        let dataEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");   
+
+        displayCalcEl.innerHTML = "4568";
+        dataEl.innerHTML = "05/12/2022";
+        timeEl.innerHTML = "00:48";
+    }
+
+    get displayCalc()
+    {
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor)
+    {
+        this._displayCalc = valor;
+    }
+
+    get dataAtual()
+    {
+        return this._currentDate;
+    }
     
+    set dataAtual(valor)
+    {
+        this._currentDate = valor;
+    }
 }
